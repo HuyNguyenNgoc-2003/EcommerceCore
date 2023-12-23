303f7284bda29900c1613d03c34c7738130c3ce7
Huy Nguyễn Ngọc <nguyenngochuy_data@outlook.com.vn>
2023-12-24 00:22:06 +0700

    danh sách sản phẩm,tìm kiếm

--- EcommerceCore/Controllers/ProductController.cs
+++ EcommerceCore/Controllers/ProductController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceCore.Data;
+using EcommerceCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceCore.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly EcommerceContext db;
+
+        public ProductController(EcommerceContext context)
+        {
+            db = context;
+        }
         public IActionResult Index(int? loai)
         {
-            return View();
+            var proDucts = db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                proDucts = proDucts.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = proDucts.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,//có thể null
+                Hinh = p.Hinh ?? "",//rỗng
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);//
         }
+        public IActionResult Search(string? query)
+        {
+            var proDucts = db.HangHoas.AsQueryable();
+            if (query != null)
+            {
+                proDucts = proDucts.Where(p => p.TenHh.Contains(query));
+            }
+            var result = proDucts.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,//có thể null
+                Hinh = p.Hinh ?? "",//rỗng
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+
     }
 }
